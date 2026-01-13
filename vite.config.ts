73457d2a1704e015adb086a6136b3a50aa56f98e
Mahika Kushwaha <mahikakushwaha@Mahikas-MacBook-Air.local>
2026-01-13 23:58:49 +0530

    url

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "guidera/chat/",
   server: {
     host: "::",
     port: 8080,
